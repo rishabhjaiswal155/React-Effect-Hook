e33a56ac9a8f16e5636dfc3ff5fd18ca2522d62f
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-06-06 15:23:33 +0530

    Counter example with both class Component and functional Component(useEffect hook with render)

---
+++ src/components/HookCounterOne.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react'
+
+function HookCounterOne() {
+    const [count,setCount]=useState(0)
+    useEffect(()=>{
+        document.title=`You Clicked ${count} times`
+    })
+  return (
+    <div>HookCounterOne<br/>
+    <button onClick={()=>setCount(count+1)}>Click {count} times</button>
+    </div>
+  )
+}
+
+export default HookCounterOne
